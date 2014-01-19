55c349afc4e36f63dd38b1e97c85a757b0b84f65
marta szotkowska <marta.szotkowska@gmail.com>
2014-01-19 12:46:47 +0100

    combine results

--- porady.rb
+++ porady.rb
@@ -10,14 +10,10 @@ get '/szukaj' do
 	@keyword = params['keyword']
 	url = "http://poradnia.pwn.pl/lista.php?szukaj=" + URI.encode(@keyword.encode("ISO-8859-2"))
 	doc = Nokogiri.HTML(open(url))
-	@result = doc.at_css('#listapytan').to_s.encode('UTF-8')
+	@result_sjp = doc.at_css('#listapytan').to_s.encode('UTF-8')
+	url = "http://www.poradnia-jezykowa.uz.zgora.pl/wordpress/?s=przymiotnik&submit=Szukaj" + URI.encode(@keyword)
+	doc = Nokogiri.HTML(open(url))
+	@result_uz = doc.at_css('#content')
 	erb :szukaj
 end
  
-get '/s' do
-	@keyword = params['keyword']
-	url = "http://www.poradnia-jezykowa.uz.zgora.pl/wordpress/?s=przymiotnik&submit=Szukaj" + URI.encode(@keyword)
-	doc = Nokogiri.HTML(open(url))
-	@result = doc.at_css('#content')
-	erb :szukaj
-end
