1eb7ee1f4c11c950d0d028416ef7e0a77ef0274c
marta szotkowska <marta.szotkowska@gmail.com>
2014-01-19 13:17:41 +0100

    add sjp

--- porady.rb
+++ porady.rb
@@ -9,9 +9,9 @@ end
 get '/szukaj' do
 	@keyword = params['keyword']
 
-	url_sjp = "http://poradnia.pwn.pl/lista.php?szukaj=" + URI.encode(@keyword.encode("ISO-8859-2"))
-	doc_sjp = Nokogiri.HTML(open(url_sjp))
-	@result_sjp = doc_sjp.at_css('#listapytan').to_s.encode('UTF-8')
+	url_poradnia = "http://poradnia.pwn.pl/lista.php?szukaj=" + URI.encode(@keyword.encode("ISO-8859-2"))
+	doc_poradnia = Nokogiri.HTML(open(url_poradnia))
+	@result_poradnia = doc_poradnia.at_css('#listapytan').to_s.encode('UTF-8')
 
 	url_uz = "http://www.poradnia-jezykowa.uz.zgora.pl/wordpress/?s=" + URI.encode(@keyword)
 	doc_uz = Nokogiri.HTML(open(url_uz))
@@ -20,6 +20,11 @@ get '/szukaj' do
 	url_wsjp = "http://www.wsjp.pl/index.php?szukaj=" + URI.encode(@keyword)
 	doc_wsjp = Nokogiri.HTML(open(url_wsjp))
 	@result_wsjp = doc_wsjp.at_css('.wyszukiwanie_wyniki .wyszukiwanie_wyniki')
+
+	url_sjp = "http://sjp.pwn.pl/szukaj/" + URI.encode(@keyword)
+	doc_sjp = Nokogiri.HTML(open(url_sjp))
+	@result_sjp = doc_sjp.at_css('#tresc').to_s.encode('UTF-8')
+
 
 	erb :szukaj
 end
